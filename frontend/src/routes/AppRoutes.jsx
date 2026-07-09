59ce08d300d59a4c18209aaa301caadca04552a7
Andreza Leal <andrezaleal.m@gmail.com>
2026-07-09 20:29:53 -0300

    feat(auth): implementa contexto de autenticação e proteção de rotas

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Cadastro from "../pages/Cadastro/Cadastro";
+import ProtectedRoute from "./ProtectedRoute";
+import Perfil from "../pages/Perfil/Perfil";
 
 export default function AppRoutes() {
   return (
@@ -11,6 +13,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/perfil" element={ <ProtectedRoute><Perfil /></ProtectedRoute>}/>
       </Routes>
     </BrowserRouter>
   );

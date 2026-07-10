43666b4b1166421a77e07ba3e47540697f3081c2
Andreza Leal <andrezaleal.m@gmail.com>
2026-07-10 02:10:09 -0300

    chore(home): remove código de teste do componente Button

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,24 +4,6 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-
-      <Button>Entrar</Button>
-
-      <Button variant="secondary">
-        Cancelar
-      </Button>
-
-      <Button variant="danger">
-        Excluir
-      </Button>
-
-      <Button variant="ghost">
-        Voltar
-      </Button>
-
-      <Button disabled>
-        Desabilitado
-      </Button>
     </div>
   );
 
